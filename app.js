a53c8fa372feaf67a00a90244f5e934d2bc9e42f
unknown <azk.akhtar@gmail.com>
2025-10-31 21:19:18 +0530

    jsx

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {id : "heading1"}, "Hello from react");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+const heading2 = ( <div id ="heading2">
+    <h1>Hello from JSX 1</h1>
+    <h1>Hello from JSX 2</h1>
+  </div>);
+
+  const root = ReactDOM.createRoot(document.getElementById('heading1'));
+  const root2 = ReactDOM.createRoot(document.getElementById('heading2'));
+  root.render(heading);
+  root2.render(heading2);
