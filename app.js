6cdb1d9ad0d7ac09ad1ac01e1a0dec6f1d9ff185
unknown <azk.akhtar@gmail.com>
2025-11-02 16:44:14 +0530

    useState Hook

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import restaurantlist from "./mockdata";
+import initalrestaurantlist from "./mockdata";
 
 // const heading = React.createElement("h1", {id : "heading1"}, "Hello from react");
 
@@ -44,81 +46,7 @@ const Restaurantcard = (props) => {
 };
 
 const Foodwebsite = () => {
-    const [restaurantlist, setRestaurantlist] = useState([
-{
-id: "20046",
-name: "Everbake",
-cloudinaryImageId: "au28pqexqycnlmskk8bl",
-locality: "Shalimar Bagh",
-areaName: "Ashok vihar",
-costForTwo: "₹500 for two",
-cuisines: [],
-avgRating: 3.5,
-veg: true,
-parentId: "4772",
-avgRatingString: "4.5",
-totalRatingsString: "132K+",
-promoted: true,
-adTrackingId: "cid=c36dcd5f-e035-4700-ac9a-ca93e7485f3b~p=0~adgrpid=c36dcd5f-e035-4700-ac9a-ca93e7485f3b#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=20046~plpr=COLLECTION~eid=67987435-354a-40bb-8b7f-15be4a852533~srvts=1762054497992~collid=80382",
-},
-{
-    id: "300087",
-name: "Shree Gopal Ji Ke Chhole Bhature (Since 1996)",
-cloudinaryImageId: "p4fn2esrcxfeeefllnjz",
-locality: "Green Park",
-areaName: "Rohini",
-costForTwo: "₹200 for two",
-cuisines: [
-"Snacks"
-],
-avgRating: 4.7,
-veg: true,
-parentId: "559265",
-avgRatingString: "4.7",
-totalRatingsString: "11K+"
-},
- {
-id: "432396",
-name: "Bansiwala",
-cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/24/8d1968db-0be4-4d01-a48b-0c89f1b195a3_432396.jpg",
-locality: "Sector 3",
-areaName: "Rohini",
-costForTwo: "₹300 for two",
-cuisines: [
-"Sweets",
-"Chaat",
-"Chinese",
-"Snacks",
-"Beverages"
-],
-avgRating: 4.2,
-veg: true,
-parentId: "6840",
-avgRatingString: "4.2",
-totalRatingsString: "2.2K+",
-},
-{
-id: "40524",
-name: "Bikanervala",
-cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/4ed4eb62-da8d-407b-834c-7e3e2b69b8cb_40524.jpg",
-locality: "Sector 10",
-areaName: "Rohini",
-costForTwo: "₹300 for two",
-cuisines: [
-"North Indian",
-"Snacks",
-"Chaat",
-"Sweets"
-],
-avgRating: 4.4,
-veg: true,
-parentId: "45936",
-avgRatingString: "4.4",
-totalRatingsString: "31K+",
-promoted: true,
-adTrackingId: "cid=34461847~p=1~adgrpid=34461847#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=40524~plpr=COLLECTION~eid=8789fd38-7ce5-4e7e-a10a-14a63a9d951d~srvts=1762059066560~collid=80382",
-}
-]);
+    const [restaurantlist, setRestaurantlist] = useState(initalrestaurantlist);
     return(
         <div>
             <Header />
